73da4645f50b56c73f52fcaa7068c247087cfeb3
CristianAcostaDuarte <101611537+CristianAcostaDuarte@users.noreply.github.com>
2022-12-01 01:45:50 -0500

    Finally working

--- delete_containers.sh
+++ delete_containers.sh
@@ -5,3 +5,9 @@ sudo docker volume ls
 var=$(sudo docker volume ls| awk '{if(NR == 2){print $2}}')
 sudo docker volume rm $var
 sudo rm -r data
+
+#En caso de que Nginx moleste o diga que el puerto 80 este ocupado usar el comando
+sudo lsof -nP | grep LISTEN
+
+# En cAaso de que sea Nginx el servicio que este molestando entonces usamos
+sudo systemctl stop nginx 
--- project/app.py
+++ project/app.py
@@ -23,120 +23,123 @@ from email.mime.text import MIMEText
 app = Flask(__name__)
 
 
-# Email configuration
-def send_email(ticker,price,date,percentage):
-    # Define the HTML document
-    html =  '''
-<html>
+# # Email configuration
+# def send_email(ticker,price,date,percentage):
+#     # Define the HTML document
+#     html =  '''
+# <html>
     
- <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
+#  <head>
+# <meta charset="UTF-8">
+# <meta name="viewport" content="width=device-width, initial-scale=1">
+# <style>
 
-/* Style the body */
-body {{
-  font-family: Arial;
-  margin: 0;
-}}
+# /* Style the body */
+# body {{
+#   font-family: Arial;
+#   margin: 0;
+# }}
 
-/* Header/Logo Title */
-.header {{
-  padding: 10px;
-  text-align: center;
-  background: #000000;
-  color: white;
-  font-size: 30px;
-}}
+# /* Header/Logo Title */
+# .header {{
+#   padding: 10px;
+#   text-align: center;
+#   background: #000000;
+#   color: white;
+#   font-size: 30px;
+# }}
 
-/* Page Content */
-.content {{
-	padding:20px;
-	color: #000000;
-	text-align: center;
-}}
+# /* Page Content */
+# .content {{
+# 	padding:20px;
+# 	color: #000000;
+# 	text-align: center;
+# }}
 
-body {{
-  background-image: url('https://cdn.pixabay.com/photo/2017/08/10/01/42/stock-market-2616931_960_720.jpg');
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: center;
-}}
+# body {{
+#   background-image: url('https://cdn.pixabay.com/photo/2017/08/10/01/42/stock-market-2616931_960_720.jpg');
+#   background-repeat: no-repeat;
+#   background-attachment: fixed;
+#   background-position: center;
+# }}
 
-footer {{
-  text-align: center;
-  padding: 3px;
-  background-color: #000000;
-  color: white;
-}}
+# footer {{
+#   text-align: center;
+#   padding: 3px;
+#   background-color: #000000;
+#   color: white;
+# }}
 
-</style>
-</head>
+# </style>
+# </head>
 
 
 
-<body>
+# <body>
 
-<div class="header">
-  <h1>Stock Alert!</h1>
-</div>
+# <div class="header">
+#   <h1>Stock Alert!</h1>
+# </div>
 
-<div class="content">
-  <h1>Ticker.</h1>
-  <h2>{}</h2>
-  <h1>Price:</h1>
-  <h2>{}</h2>
-  <h1>Date and hour:</h1>
-  <h2>{}</h2>
-  <h1>Comparisson with previous prices:</h1>
-  <h2>{}%</h2>
+# <div class="content">
+#   <h1>Ticker.</h1>
+#   <h2>{}</h2>
+#   <h1>Price:</h1>
+#   <h2>{}</h2>
+#   <h1>Date and hour:</h1>
+#   <h2>{}</h2>
+#   <h1>Comparisson with previous prices:</h1>
+#   <h2>{}%</h2>
 
-</div>
+# </div>
 
-</body>
+# </body>
 
-<footer>
-	<p>Author: Trading Devs</p>
-	<p><a href="tradingdevs@example.com">tradingdevs@example.com</a></p>
-</footer>
+# <footer>
+# 	<p>Author: Trading Devs</p>
+# 	<p><a href="tradingdevs@example.com">tradingdevs@example.com</a></p>
+# </footer>
 
   
-</html>
-'''
-    html=html.format(ticker,price,date,percentage)
+# </html>
+# '''
+#     html=html.format(ticker,price,date,percentage)
 
-    # Set up the email addresses and password. Please replace below with your email address and password
-    email_from = 'jp232277@gmail.com'
-    password = 'udtxiejvfapmsqpz'
-    email_to = 'jp232277@gmail.com'
+#     # Set up the email addresses and password. Please replace below with your email address and password
+#     email_from = 'jp232277@gmail.com'
+#     password = 'udtxiejvfapmsqpz'
+#     email_to = 'jp232277@gmail.com'
 
-    # Generate today's date to be included in the email Subject
-    date_str = pd.Timestamp.today().strftime('%Y-%m-%d')
+#     # Generate today's date to be included in the email Subject
+#     date_str = pd.Timestamp.today().strftime('%Y-%m-%d')
 
-    # Create a MIMEMultipart class, and set up the From, To, Subject fields
-    email_message = MIMEMultipart()
-    email_message['From'] = email_from
-    email_message['To'] = email_to
-    email_message['Subject'] = f'Alert Stock!! - {date_str}'
+#     # Create a MIMEMultipart class, and set up the From, To, Subject fields
+#     email_message = MIMEMultipart()
+#     email_message['From'] = email_from
+#     email_message['To'] = email_to
+#     email_message['Subject'] = f'Alert Stock!! - {date_str}'
 
-    # Attach the html doc defined earlier, as a MIMEText html content type to the MIME message
-    email_message.attach(MIMEText(html, "html"))
-    # Convert it as a string
-    email_string = email_message.as_string()
+#     # Attach the html doc defined earlier, as a MIMEText html content type to the MIME message
+#     email_message.attach(MIMEText(html, "html"))
+#     # Convert it as a string
+#     email_string = email_message.as_string()
 
-    # Connect to the Gmail SMTP server and Send Email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(email_from, password)
-        server.sendmail(email_from, email_to, email_string)
+#     # Connect to the Gmail SMTP server and Send Email
+#     context = ssl.create_default_context()
+#     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+#         server.login(email_from, password)
+#         server.sendmail(email_from, email_to, email_string)
 
 
 # Conection = MongoDB(app)
-client = MongoClient(host='test_mongodb',
-                     port=27017,
-                     username='root',
-                     password='pass',
-                     authSource='admin')
+# client = MongoClient(host='mongo',
+#                      port=27017,
+#                      username='root',
+#                      password='pass',
+#                      authSource='admin')
+
+client = MongoClient('mongodb://mongo:27017/')
+
 
 database = client['stockdb']
 
@@ -165,7 +168,7 @@ current_date = (str(today) + ' / ' + str(current_time))
 def insert_dava():
     print('Endava')
     Endava = requests.get(
-        'https://financialmodelingprep.com/api/v3/quote-short/DAVA?apikey=130539b6c85bda9f602da3c028aff9ba')
+        'https://financialmodelingprep.com/api/v3/quote-short/DAVA?apikey=2c18aef9c283317435d76672c51df35d')
     Endava = Endava.json()
     Endava[0]['date'] = current_date
     dava.insert_many(Endava)
@@ -174,7 +177,7 @@ def insert_dava():
 def insert_ec():
     print('Ecopetrol')
     Ecopetrol = requests.get(
-        'https://financialmodelingprep.com/api/v3/quote-short/EC?apikey=130539b6c85bda9f602da3c028aff9ba')
+        'https://financialmodelingprep.com/api/v3/quote-short/EC?apikey=2c18aef9c283317435d76672c51df35d')
     Ecopetrol = Ecopetrol.json()
     Ecopetrol[0]['date'] = current_date
     ec.insert_many(Ecopetrol)
@@ -183,7 +186,7 @@ def insert_ec():
 def insert_ibm():
     print('IBM')
     IBM = requests.get(
-        'https://financialmodelingprep.com/api/v3/quote-short/IBM?apikey=130539b6c85bda9f602da3c028aff9ba')
+        'https://financialmodelingprep.com/api/v3/quote-short/IBM?apikey=2c18aef9c283317435d76672c51df35d')
     IBM = IBM.json()
     IBM[0]['date'] = current_date
     ibm.insert_many(IBM)
@@ -192,7 +195,7 @@ def insert_ibm():
 def insert_btc():
     print('Bitcoin')
     BTC = requests.get(
-        'https://financialmodelingprep.com/api/v3/quote-short/BTC-USD?apikey=130539b6c85bda9f602da3c028aff9ba')
+        'https://financialmodelingprep.com/api/v3/quote-short/BTC-USD?apikey=2c18aef9c283317435d76672c51df35d')
     BTC = BTC.json()
     BTC[0]['date'] = current_date
     btc.insert_many(BTC)
@@ -201,7 +204,7 @@ def insert_btc():
 def insert_eth():
     print('Ethereum')
     ETH = requests.get(
-        'https://financialmodelingprep.com/api/v3/quote-short/ETH-USD?apikey=130539b6c85bda9f602da3c028aff9ba')
+        'https://financialmodelingprep.com/api/v3/quote-short/ETH-USD?apikey=2c18aef9c283317435d76672c51df35d')
     ETH = ETH.json()
     ETH[0]['date'] = current_date
     eth.insert_many(ETH)
@@ -210,7 +213,7 @@ def insert_eth():
 def insert_ada():
     print('Cardano')
     ADA = requests.get(
-        'https://financialmodelingprep.com/api/v3/quote-short/ADA-USD?apikey=130539b6c85bda9f602da3c028aff9ba')
+        'https://financialmodelingprep.com/api/v3/quote-short/ADA-USD?apikey=2c18aef9c283317435d76672c51df35d')
     ADA = ADA.json()
     ADA[0]['date'] = current_date
     ada.insert_many(ADA)
@@ -262,68 +265,68 @@ def get_ada():
 # Find the average of each collection
 
 
-def Average(lst):
-    return sum(lst) / len(lst)
+# def Average(lst):
+#     return sum(lst) / len(lst)
 
 # Email function
 
 
-def stock_mail():
+# def stock_mail():
 
     
-    dava_df = get_dava()
-    current_dava = dava_df['price'].values[-1]
-    date_dava = dava_df['date'].values[-1]
-    mean_dava = round(Average(dava_df['price'].values[:-1]), 2)
-    percent_dava = round(((current_dava-mean_dava)/mean_dava)*100,2)
+#     dava_df = get_dava()
+#     current_dava = dava_df['price'].values[-1]
+#     date_dava = dava_df['date'].values[-1]
+#     mean_dava = round(Average(dava_df['price'].values[:-1]), 2)
+#     percent_dava = round(((current_dava-mean_dava)/mean_dava)*100,2)
 
-    if (percent_dava > 20 or percent_dava < -20):
-        send_email('DAVA', current_dava,  date_dava , percent_dava)
+#     if (percent_dava > 20 or percent_dava < -20):
+#         send_email('DAVA', current_dava,  date_dava , percent_dava)
 
-    ec_df = get_ec()
-    current_ec = ec_df['price'].values[-1]
-    date_ec = ec_df['date'].values[-1]
-    mean_ec = round(Average(ec_df['price'].values[:-1]), 2)
-    percent_ec =round(((current_ec-mean_ec)/mean_ec)*100,2)
+#     ec_df = get_ec()
+#     current_ec = ec_df['price'].values[-1]
+#     date_ec = ec_df['date'].values[-1]
+#     mean_ec = round(Average(ec_df['price'].values[:-1]), 2)
+#     percent_ec =round(((current_ec-mean_ec)/mean_ec)*100,2)
 
-    if (percent_ec > 20 or percent_ec < -20):
-        send_email('EC', current_ec,  date_ec , percent_ec)
+#     if (percent_ec > 20 or percent_ec < -20):
+#         send_email('EC', current_ec,  date_ec , percent_ec)
 
-    ibm_df = get_ibm()
-    current_ibm = ibm_df['price'].values[-1]
-    date_ibm = ibm_df['date'].values[-1]
-    mean_ibm = round(Average(ibm_df['price'].values[:-1]), 2)
-    percent_ibm = round(((current_ibm-mean_ibm)/mean_ibm)*100,2)
+#     ibm_df = get_ibm()
+#     current_ibm = ibm_df['price'].values[-1]
+#     date_ibm = ibm_df['date'].values[-1]
+#     mean_ibm = round(Average(ibm_df['price'].values[:-1]), 2)
+#     percent_ibm = round(((current_ibm-mean_ibm)/mean_ibm)*100,2)
 
-    if (percent_ibm > 20 or percent_ibm < -20):
-        send_email('IBM', current_ibm,  date_ibm , percent_ibm)
+#     if (percent_ibm > 20 or percent_ibm < -20):
+#         send_email('IBM', current_ibm,  date_ibm , percent_ibm)
 
-    btc_df = get_btc()
-    current_btc = btc_df['price'].values[-1]
-    date_btc = btc_df['date'].values[-1]
-    mean_btc = round(Average(btc_df['price'].values[:-1]), 2)
-    percent_btc = round(((current_btc-mean_btc)/mean_btc)*100,2)
+#     btc_df = get_btc()
+#     current_btc = btc_df['price'].values[-1]
+#     date_btc = btc_df['date'].values[-1]
+#     mean_btc = round(Average(btc_df['price'].values[:-1]), 2)
+#     percent_btc = round(((current_btc-mean_btc)/mean_btc)*100,2)
 
-    if (percent_btc > 20 or percent_btc < -20):
-        send_email('BTC', current_btc,  date_btc , percent_btc)
+#     if (percent_btc > 20 or percent_btc < -20):
+#         send_email('BTC', current_btc,  date_btc , percent_btc)
 
-    eth_df = get_eth()
-    current_eth = eth_df['price'].values[-1]
-    date_eth = eth_df['date'].values[-1]
-    mean_eth = round(Average(eth_df['price'].values[:-1]), 2)
-    percent_eth = round(((current_eth-mean_eth)/mean_eth)*100,2)
+#     eth_df = get_eth()
+#     current_eth = eth_df['price'].values[-1]
+#     date_eth = eth_df['date'].values[-1]
+#     mean_eth = round(Average(eth_df['price'].values[:-1]), 2)
+#     percent_eth = round(((current_eth-mean_eth)/mean_eth)*100,2)
 
-    if (percent_eth > 20 or percent_eth < -20):
-        send_email('ETH', current_eth,  date_eth , percent_eth)
+#     if (percent_eth > 20 or percent_eth < -20):
+#         send_email('ETH', current_eth,  date_eth , percent_eth)
         
-    ada_df = get_ada()
-    current_ada = ada_df['price'].values[-1]
-    date_ada = ada_df['date'].values[-1]
-    mean_ada = round(Average(ada_df['price'].values[:-1]), 2)
-    percent_ada = round(((current_ada-mean_ada)/mean_ada)*100,2)
+#     ada_df = get_ada()
+#     current_ada = ada_df['price'].values[-1]
+#     date_ada = ada_df['date'].values[-1]
+#     mean_ada = round(Average(ada_df['price'].values[:-1]), 2)
+#     percent_ada = round(((current_ada-mean_ada)/mean_ada)*100,2)
 
-    if (percent_ada > 20 or percent_ada < -20):
-        send_email('ADA', current_ada,  date_ada , percent_ada)
+#     if (percent_ada > 20 or percent_ada < -20):
+#         send_email('ADA', current_ada,  date_ada , percent_ada)
 
     
 
@@ -334,20 +337,20 @@ def updateDatabase():
 
 
 # 300 = 5 minutes
-def countdown(countDownTime=300):
+def countdown(countDownTime=60):
     global my_timer
     my_timer = countDownTime
     while my_timer > 0:
         time.sleep(1)
         my_timer -= 1
     updateDatabase()
-    insert_dava()
-    insert_ec()
-    insert_ibm()
-    insert_btc()
-    insert_eth()
-    insert_ada()
-    stock_mail()
+    # insert_dava()
+    # insert_ec()
+    # insert_ibm()
+    # insert_btc()
+    # insert_eth()
+    # insert_ada()
+    # stock_mail()
     countdown(countDownTime)
 
 
@@ -361,10 +364,16 @@ def getDocuments(currency='dava'):
 
 @app.route("/")
 def hello_world():
+    insert_dava()
+    insert_ec()
+    insert_ibm()
+    insert_btc()
+    insert_eth()
+    insert_ada()
     dava_chart=get_dava()
-    x=list(dava_chart['date'].values)[:2]
+    x=list(dava_chart['date'].values)
     print(x)
-    y=list(dava_chart['price'].values)[:2]
+    y=list(dava_chart['price'].values)
     print(y)
     return render_template('result.html', dates=x, stocks=y)    
 
@@ -376,38 +385,38 @@ def index(currency='dava'):
     if currency == 'dava':
         currencyName = 'Endava'
         dava_chart=get_dava()
-        x=list(dava_chart['date'].values)[-11:]
-        y=list(dava_chart['price'].values)[-11:]
+        x=list(dava_chart['date'].values)
+        y=list(dava_chart['price'].values)
 
     if currency == 'ec':
         currencyName = 'Ecopetrol'
         ec_chart=get_ec()
-        x=list(ec_chart['date'].values)[-11:]
-        y=list(ec_chart['price'].values)[-11:]
+        x=list(ec_chart['date'].values)
+        y=list(ec_chart['price'].values)
 
     if currency == 'ibm':
         currencyName = 'IBM'
         ibm_chart=get_ibm()
-        x=list(ibm_chart['date'].values)[-11:]
-        y=list(ibm_chart['price'].values)[-11:]
+        x=list(ibm_chart['date'].values)
+        y=list(ibm_chart['price'].values)
 
     if currency == 'btc':
         currencyName = 'Bitcoin'
         btc_chart=get_btc()
-        x=list(btc_chart['date'].values)[-11:]
-        y=list(btc_chart['price'].values)[-11:]
+        x=list(btc_chart['date'].values)
+        y=list(btc_chart['price'].values)
 
     if currency == 'eth':
         currencyName = 'Ethereum'
         eth_chart=get_eth()
-        x=list(eth_chart['date'].values)[-11:]
-        y=list(eth_chart['price'].values)[-11:]
+        x=list(eth_chart['date'].values)
+        y=list(eth_chart['price'].values)
 
     if currency == 'ada':
         currencyName = 'Cardano'
         ada_chart=get_ada()
-        x=list(ada_chart['date'].values)[-11:]
-        y=list(ada_chart['price'].values)[-11:]
+        x=list(ada_chart['date'].values)
+        y=list(ada_chart['price'].values)
 
     data = {
         'currency': "💱 " + currencyName,
